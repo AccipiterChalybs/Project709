1654e7c427e6301208c3bdaaf8dccf6e59d6196d
Alex Hawker <pumpkinpi64@gmail.com>
2014-01-24 20:18:14 -0800

    Trying to get objects working

---
+++ Unity Project/Assets/Script/HeightFinder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeightFinder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<CharacterController>().height = transform.FindChild("Character").collider.bounds.size.y;
+		print (transform.Find ("Character").collider.bounds.size.y);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
